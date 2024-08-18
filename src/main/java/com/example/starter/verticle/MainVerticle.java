c00f7e9e788801ce796f68a35cf875645ed67576
atulk1729 <atulkumar1729@gmail.com>
2024-08-18 18:12:42 +0530

    Added code to return ip of client

--- src/main/java/com/example/starter/verticle/MainVerticle.java
+++ src/main/java/com/example/starter/verticle/MainVerticle.java
@@ -2,6 +2,7 @@ package com.example.starter.verticle;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpServerRequest;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -10,7 +11,7 @@ public class MainVerticle extends AbstractVerticle {
         vertx.createHttpServer().requestHandler(req -> {
             req.response()
                     .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x!");
+                    .end("Hello from Vert.x to the person with ip: " + getClientIp(req));
         }).listen(8888).onComplete(http -> {
             if (http.succeeded()) {
                 startPromise.complete();
@@ -19,5 +20,15 @@ public class MainVerticle extends AbstractVerticle {
                 startPromise.fail(http.cause());
             }
         });
+    }
+
+    private String getClientIp(HttpServerRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.isEmpty()) {
+            ip = request.remoteAddress().host();
+        } else {
+            ip = ip.split(",")[0];
+        }
+        return ip;
     }
 }
